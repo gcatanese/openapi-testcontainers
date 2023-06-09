76c169c7fe1ab24a710e7538e4dc353d139c2d39
Beppe Catanese <gcatanese@yahoo.com>
2023-06-09 10:22:40 +0200

    Store build timestamp

--- src/main/java/com/tweesky/cloudtools/codegen/TestContainersCodegen.java
+++ src/main/java/com/tweesky/cloudtools/codegen/TestContainersCodegen.java
@@ -36,6 +36,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class TestContainersCodegen extends AbstractGoCodegen {
